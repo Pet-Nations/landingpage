8abfdb0ca68bb56beae0714970852b5064eddbe0
chakerioto <chakerioto@gmail.com>
2024-01-28 13:18:34 +0700

    responsive Vision Map

--- app/components/main-footer/index.tsx
+++ app/components/main-footer/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const MainFooter = ({ page }: Props) => {
   return (
-    <div className="h-10 bg-dark-main relative z-[5000]">
+    <div className="h-10 bg-dark-main  z-[5000] fixed bottom-0 w-full">
       <BackBTN page={page} />
 
       <p className="h-full flex items-center justify-center text-sm leading-[26px] text-[#FFF4E9]">
